7370eabad8e4fae5d5802dd9513ec230a6d193ef
JJYDXFS <1043804998@qq.com>
2021-06-09 11:42:03 +0800

    add readme and raw code

---
+++ README.md
@@ -0,0 +1,29 @@
+# Flask 常用代码 生成器
+
+[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+
+
+
+因为 [Flask](https://flask.palletsprojects.com/en/2.0.x/) 使用方便，总会时不时用它进行一些后端开发。但每次代码都基本是固定的格式，想用时就要打开笔记里或者以前开发的项目里Ctrl C + Ctrl V。觉得有点麻烦  
+
+
+
+所以编一个傻瓜代码生成器吧，把经常会用到的demo写进去。用的时候直接生成似乎就方便一些  
+
+
+
+目前收录demo如下
+
+\- basic flask demo | 最基础的flask模板，可直接运行
+
+\- get demo | GET接口模板，附带一个获取参数的方法
+
+\- post demo | POST接口模板，附带一个获取文件的方法
+
+\- render demo | 渲染页面模板，附带一个重定向demo
+
+\- mysql damo (pymysql) | 基于pymysql库，支持 MySQL 5.7 和 8.0
+
+## 运行示例
+
+![2021-06-09_113448](https://jjydxfs.oss-cn-beijing.aliyuncs.com/md/2021-06-09_113448.jpg)
---
+++ flask_code_generator.py
@@ -0,0 +1,157 @@
+"""
+Flask 常用代码 生成器
+"""
+
+def choice_menu():
+    """
+    选项菜单页
+    """
+    print("---------------------------------------------------------------")
+    print("在以下选项中选择一个demo：")
+    print("1. basic flask demo",end="\t\t")
+    print("2. get demo")
+    print("3. post demo",end="\t\t\t")
+    print("4. render demo")
+    print("5. mysql damo (pymysql)",end="\t\t")
+    print("6. 退出 EXIT")
+
+def flask_template():
+    """
+    基础flask模板
+    """
+    template="""
+from flask import Flask, request, render_template, jsonify, redirect
+from flask_cors import *
+import pymysql
+import json
+import copy
+
+# 指定静态文件目录
+app = Flask(__name__,static_url_path='/static/',template_folder='templates')
+# 允许跨域访问
+CORS(app, resources=r'/*')
+
+# 主页
+@app.route('/')
+def index():
+    return "Hi Flask!"
+
+if __name__ == '__main__':
+    app.debug=True  # 默认开启debug模式
+    # host=0.0.0.0 port=5000
+    app.run('0.0.0.0', 5000)
+"""
+    print(template)
+
+def get_demo():
+    """
+    GET接口模板
+    """
+    name=input("请输入GET接口名称：")
+    template="""
+@app.route('/api/{name}', methods=['GET'])
+def {name}():
+    '''
+    {name}
+    '''
+    # request.args.get 用于获取GET所传参数
+    value=request.args.get('key')
+    
+    return json.dumps({{'result':'success'}},ensure_ascii=False)
+""".format(name=name)
+
+    print(template)
+
+def post_demo():
+    """
+    POST接口模板
+    """
+    name=input("请输入POST接口名称：")
+    post_template="""
+@app.route('/api/{name}', methods=['POST'])
+def {name}():
+    '''
+    {name}
+    '''
+    value=request.form['key'] # 获取表单中的参数
+    # img = request.files['file']   # 获取表单中的文件
+    
+    return json.dumps({{'result':'success'}},ensure_ascii=False)    
+""".format(name=name)
+
+    print(post_template)
+
+def mysql_damo():
+    '''
+    pymysql操纵MySQL demo
+    '''
+    flag=input("MySQL 5.7(a) or MySQL 8.0(b) or neither(c)?:")
+    if flag == 'a':
+        connect_template="""
+db = pymysql.connect(host='主机ip', user='用户名', password='密码', database='数据库名')\n
+"""
+    elif flag == 'b':
+        connect_template="""
+db = pymysql.connect("主机ip","用户名","密码","数据库名" )\n
+"""
+    elif flag == 'c':
+        print('无对应demo！')
+        return
+    else:
+        print('输入有误！')
+        return
+    
+    mysql_template=connect_template+"""
+cursor = db.cursor()    # 创建一个游标对象
+
+sql=\"\"\"要执行的SQL语句;\"\"\"
+
+cursor.execute(sql) # 执行语句
+data=cursor.fetchall()  # 获取元组形式查询结果 .fatchone()方法获取单条数据
+db.close() # 关闭连接
+"""
+    print(mysql_template)
+
+
+def render_demo():
+    '''
+    模板渲染demo
+    '''
+    name=input("输入路由的名字：")
+    render_template="""
+@app.route('/{name}')
+def {name}():
+    '''
+    {name}
+    '''
+    key='hi'    # 传给模板的参数
+
+    # return redirect('http://www.baidu.com') # 重定向到任意url
+    return render_template("html-template-name.html",title = key)   # 静态文件渲染模板样式并传参
+""".format(name=name)
+
+    print(render_template)
+
+def bye():
+    print("Bye!")
+    exit(0)
+
+def default():
+    print("序号输入有误")
+
+switch = {'1': flask_template,
+    '2': get_demo,
+    '3': post_demo,
+    '4': render_demo,
+    '5': mysql_damo,
+    '6': bye,
+}
+
+if __name__=="__main__":
+    print("-----------------欢迎来到flask代码傻瓜生成器！-----------------")
+    print("----------Welcome to naive demo generator for flask----------")
+    while True:
+        choice_menu()
+        choice = input("请输入demo序号：")
+        print("---------------------------------------------------------------")
+        switch.get(choice, default)()
